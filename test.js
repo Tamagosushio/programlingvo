0405eb64c06553e68e93ac7e86833af097981d02
Tamagosushio <mitachi.ts75@gmail.com>
2025-05-14 09:16:14 +0900

    [update] エラー出力を分かりやすく変更

--- test.js
+++ test.js
@@ -2,6 +2,17 @@ const fs = require("fs");
 const parser = require("./parser");
 const code = fs.readFileSync("./input.espr", "utf-8");
 
-const ast = parser.parse(code);
-console.dir(ast, {depth: null});
+const tracer = {
+  trace: (event) => {
+    const {type, rule, location} = event;
+    console.log(`${type} ${rule} at ${location.start.offset}`);
+  },
+};
+
+try{
+  const ast = parser.parse(code);
+  console.dir(ast, {depth: null});
+}catch(e){
+  console.error(e.message);
+}
 
